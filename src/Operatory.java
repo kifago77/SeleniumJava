042bef8ec3bc7fbba6928148afab19807d457048
Marcin SeleniumJava <mf.kwiatkowski@wp.pl>
2022-06-22 12:56:14 +0200

    My changes

---
+++ src/Operatory.java
@@ -0,0 +1,24 @@
+public class Operatory {
+
+    public static void main(String[] args) {
+        int firstNumber = 25;
+        int secondNumber = 4;
+
+//Dodawanie(addition)
+        int addition = firstNumber + secondNumber;
+        System.out.println(addition);
+//Odejmowanie(subtraction)
+        int subtraction = firstNumber - secondNumber;
+        System.out.println(subtraction);
+//Mnożenie (multiplication)
+        int multiplication = firstNumber * secondNumber;
+        System.out.println(multiplication);
+//Dzielenie(division)
+        int division = firstNumber / secondNumber;
+        System.out.println(division);
+//Reszta z dzielenia (mod)
+        int mod = firstNumber % secondNumber;
+        System.out.println(mod);
+
+    }
+}
